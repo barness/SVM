decaac1ab7dbfcab913663ad9bb99624e0e98058
Elijah Pineda <epinedaleyva@student.unimelb.edu.au>
2019-04-08 23:00:08 -0400

    added some shit

--- sliding_window_nate.py
+++ sliding_window_nate.py
@@ -1,25 +1,29 @@
 import pandas as pd
+import sliding_window as sw
+
+width = 5
+window_separation = 14
+
 
 def get_data(filename, sheetname):
-    
-    df = pd.read_excel(filename, sheet_name = sheetname)
-    
-    ticker_list = [ticker for ticker in df.columns[1:]]
+    df = pd.read_excel(filename, sheet_name=sheetname)
 
     dates = df["date"]
-
+    ticker_list = [ticker for ticker in df.columns[1:]]
 
     for ticker in ticker_list:
-    	
-    	prices = df[ticker]
+        prices = df[ticker]
+        window_trailing = sw.MovingAverage(width)
+        window_leading = sw.MovingAverage(width)
 
-    # 
+        for price in prices:
+            price
 
-    # 
 
     return ticker_list
 
 def parse_data(list_of_tickers):
+    pass
 
 
 if __name__ == '__main__':
